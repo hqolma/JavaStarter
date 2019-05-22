c190079fddc3e584e70dc42c442d62231a32c177
Alex Lvovych <admin@olmawritings.com>
2019-05-22 15:06:15 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Завдання номер один");
+        System.out.println("Завдання номер два");
+        System.out.println("Завдання номер три");
+
+    }
+}
