2c888438cbd22fce335d91925795dc7ad0bfa195
Alex Lvovych <admin@olmawritings.com>
2019-05-28 12:30:04 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Завдання номер один");
-        System.out.println("Завдання номер два");
-        System.out.println("Завдання номер три");
+        System.out.println("Завдання Граю. Вгадати букву від A-Z: Виберіть 1");
+        System.out.println("Завдання номер два: In process...");
+        System.out.println("Завдання номер три: In process...");
+
+        
 
     }
 }
